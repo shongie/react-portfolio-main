7422f666fb44f66e39c1948bdc83099779235991
shongie <116272145+shongie@users.noreply.github.com>
2022-11-24 09:41:32 +0200

    my portfolio changes

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ import xongipic from '../../src/img/xongi-pic.jpg';
 class About extends Component {
   render() {
     if (this.props.sharedBasicInfo) {
-      var profilepic = "../public/images/xongi-pic.jpg" + this.props.sharedBasicInfo.image;
+      // var profilepic = "../public/images/xongi-pic.jpg" + this.props.sharedBasicInfo.image;
     }
     if (this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.about;
